8c6fdbca991e886bc3a9be91b9b91f081259d6fd
Thanushanth <Thanushanth@cogntix.com>
2026-04-17 07:46:18 +0530

    Update image remote patterns in next.config.ts for improved configuration

--- frontend/customer-v1/next.config.ts
+++ frontend/customer-v1/next.config.ts
@@ -5,23 +5,35 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'somerville-mobile-stage.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'somerville-mobile-dev.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "api.somervillemobile.com.au",
+        port: "",
+        pathname: "/**",
       },
       {
-         protocol: 'https',
-         hostname: 'images.unsplash.com',
-         port: '',
-         pathname: '/**',
-      }
+        protocol: "https",
+        hostname: "somerville-mobile-stage.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "prod-somervillemobile.s3.ap-southeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
